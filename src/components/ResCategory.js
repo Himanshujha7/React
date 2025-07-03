53c9c6218e26766809b2ac5463de338595611924
Himanshujha7 <tigmanshujah@gmail.com>
2025-07-04 00:48:26 +0530

    created context api

--- src/components/ResCategory.js
+++ src/components/ResCategory.js
@@ -2,25 +2,23 @@
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import ItemList from "./ItemList";
 import { useState } from "react";
-const ResCategory = ({data}) => {
-    const [isActive, setIsActive] = useState(false);
-
-    const handleToggle =() => {
-        setIsActive(!isActive);
+const ResCategory = ({data, showItems, setIsActive}) => {
+    const handleToggle = () =>{
+        setIsActive();
     }
     console.log(data);
   return (
     <div>
         <div className="flex flex-col gap-10 items-center justify-center bg-white">
-            <button onClick={handleToggle}  className="flex justify-between w-full mx-auto my-4 bg-white px-4 gap-10 p-5 ">
+            <button  onClick={handleToggle} className="flex justify-between w-full mx-auto my-4 bg-white px-4 gap-10 p-5 ">
                 <span className="font-bold text-2xl" >
                     {data.title}
                     ({data.itemCards.length})
                 </span>
-                {isActive? <FaChevronUp className="text-gray-500 text-2xl" /> : <FaChevronDown className="text-gray-500 text-2xl" />}
+                {showItems? <FaChevronUp className="text-gray-500 text-2xl" /> : <FaChevronDown className="text-gray-500 text-2xl" />}
             </button>
             <div >
-                {isActive && 
+                {showItems && 
                     <ItemList items={data.itemCards}/>}
             </div>
         </div>
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import useRestaurantMenu from "../utility/useRestaurantMenu";
 import ResCategory from "./ResCategory";
 import {Shimmer} from "./Shimmer";
@@ -5,6 +6,8 @@ import { useParams } from "react-router";
 
 
 const RestaurantMenu = () => {
+    const [isActive, setIsActive] = useState(null);
+
 
     const {resId} = useParams();
 
@@ -16,8 +19,6 @@ const RestaurantMenu = () => {
     }
        
     const {name, cuisines, costForTwoMessage, city, sla, avgRating, areaName} = resMenu?.cards[2]?.card?.card?.info || {};
-
-    const {itemCards}=resMenu?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card || {};
     
     
 
@@ -39,14 +40,21 @@ const RestaurantMenu = () => {
                 
                 <p className="bg-green-500 font-medium text-amber-50 rounded-2xl m-2 px-2 w-10 flex items-center justify-center">{avgRating}</p>
                 <p className="font-bold px-2 py-1">{costForTwoMessage}</p>
-                <p className="px-2 py-1 font-medium text-slate-700">Delivery Time:{sla?.deliveryTime}</p>
+                <p className="px-2 py-1 font-medium text-slate-700">Delivery Time:{sla.deliveryTime}</p>
             </div>
 
             <h2 className="font-bold text-3xl m-8 flex items-start justify-start">Deals for you</h2>
             <div className="w-1/2 bg-gray-100">
                 
                 {
-                    categories.map((category) => (<ResCategory key={category?.card?.card.categoryId} data={category?.card?.card} />))
+                    categories.map((category, index) => (
+                        <ResCategory
+                            showItems={isActive===index}
+                            setIsActive={()=>setIsActive(isActive === index ? null : index)}
+                            key={category?.card?.card.categoryId}
+                            data={category?.card?.card}
+                        />
+                    ))
                 }
             </div>
         </div>
